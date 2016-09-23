0775f06e9f1da99ed8c31eeb7a1188f80fe47cb0
Anthony Dmitriyev <antstorm@gmail.com>
2016-09-23 14:54:05 +0100

    Add enabled flag for triggering sends

--- lib/hosted_graphite.rb
+++ lib/hosted_graphite.rb
@@ -6,11 +6,9 @@ require 'hosted_graphite/protocol'
 module HostedGraphite
   extend Forwardable
   extend self
-  attr_reader :namespace, :protocol
-  attr_writer :namespace, :api_key
 
-  def_delegators :protocol, :send_metric
-  module_function :send_metric
+  attr_reader :namespace, :enabled, :protocol
+  attr_writer :namespace, :enabled, :api_key
 
   class MissingAPIKey < StandardError
     def initialize
@@ -21,6 +19,15 @@ module HostedGraphite
   def api_key
     @api_key ||= ENV['HOSTEDGRAPHITE_APIKEY']
   end
+
+  def enabled?
+    @enabled.nil? ? true : @enabled
+  end
+
+  def send_metric(*args)
+    protocol.send_metric(*args) if enabled?
+  end
+  module_function :send_metric
 
    def protocol=(protocol)
     protocol   = protocol.to_s.downcase
